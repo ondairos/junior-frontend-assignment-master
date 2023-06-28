86abe94c3aa14b464bdb7bcd3bcacb5390edb08f
Ondairos <ondairos@gmail.com>
2023-06-28 11:43:31 +0300

    used async await for api call in store

--- src/store/index.js
+++ src/store/index.js
@@ -23,16 +23,11 @@ export default new Vuex.Store({
             state.articles = articles
         },
     },
-
     actions: {
-        getArticles({ commit }) {
-            axios
-                // limit to 20 articles
-                .get('https://api.spaceflightnewsapi.net/v4/articles/?has_event=true&has_launch=true&limit=20')
-                .then(response => {
-                    const articles = response.data.results;
-                    commit('setArticles', articles);
-                })
+        async getArticles({ commit }) {
+            const response = await axios.get('https://api.spaceflightnewsapi.net/v4/articles/?has_event=true&has_launch=true&limit=20');
+            const articles = response.data.results
+            commit('setArticles', articles)
         }
     },
 })
